b29aa2153a82a899ad3237bccd9ffd9533f4a9b3
Mathieu <mathieu.0598@gmail.com>
2020-04-20 17:45:02 +0200

    Update chatbot conf

--- assets/js/botpress.js
+++ assets/js/botpress.js
@@ -1,46 +1,44 @@
 window.addEventListener("load", function() {
-
-    console.log('load');
-    console.log(document.getElementById('mainContainer'))
-
     if (window.botpressWebChat) {
-        window.botpressWebChat.init({
-            host: "https://botpress.apps.ocp.lab-nxtit.com",
-            botId: "botin"
-        })
+      window.botpressWebChat.init({
+        host: "https://botpress.apps.ocp.lab-nxtit.com",
+        botId: "botpress-stable",
+        //extraStylesheet: "http://website.com/custom.css",
+      })
   
-        function getCookie(name) {
-            var value = "; " + document.cookie;
-            var parts = value.split("; " + name + "=");
-            if (parts.length == 2) return parts.pop().split(";").shift();
-        }
+      function getCookie(name) {
+        var value = "; " + document.cookie;
+        var parts = value.split("; " + name + "=");
+        if (parts.length == 2) return parts.pop().split(";").shift();
+      }
   
-        const pageType = "homePage" // or whatever you want, like : "homePage", "contactPage", etc...
+      const pageType = 'homePage'
   
-        window.addEventListener("message", function(event) {
-            
-            if (event.data.name === 'webchatLoaded') {
+      window.addEventListener("message", function(event) {
+        if (event.data.name === 'webchatLoaded') {
+          // Do nothing
+        } else if (event.data.message_type === "visit") {
           // Reset session
-                window.botpressWebChat.sendEvent({ type: "session_reset"}) 
-
-            } else if (event.data.message_type === "visit") {
-                
-                const firstEvent = {}
-                firstEvent.type = pageType
-
-                if (pageType !== 'loginPage') {
-                    // Read Keycloak token
-                    const token = getCookie('keycloak_token')
-                    if (token) {
-                        // Connected
-                        firstEvent.type = "keycloak_jwt"
-                        firstEvent.value = token
-                    }
-                }
-  
-                // Send first event that is going to trigger welcome message
-                window.botpressWebChat.sendEvent(firstEvent)
+          window.botpressWebChat.sendEvent({ type: "session_reset" }) 
+          
+          // Build first event object
+          const firstEvent = {}
+          firstEvent.type = pageType
+          if (pageType !== 'loginPage') {
+            // Read Keycloak token
+            const token = getCookie('keycloak_token')
+            if (token) {
+              // Connected
+              firstEvent.type = "keycloak_jwt"
+              firstEvent.value = token
+            } else {
+              firstEvent.type = "keycloak_disconnect"
             }
-        })
+          }
+  
+          // Send first event that is going to trigger welcome message
+          window.botpressWebChat.sendEvent(firstEvent)
+        }
+      })
     }
-})
+  })
