49737d06ba86eed200e12f0b5a30cc0a9679498c
Mathieu SERRA <mathieu.serra@socgen.com>
2020-04-23 10:02:21 +0200

    Fix path depend on lanagugage bug

--- assets/js/botpress.js
+++ assets/js/botpress.js
@@ -1,4 +1,50 @@
 window.addEventListener("load", function() {
+  if (window.botpressWebChat) {
+    window.botpressWebChat.init({
+      host: "https://botpress.apps.ocp.lab-nxtit.com",
+      botId: "botpress-stable",
+    })
+
+    function getScript() {
+      return document.getElementById('botpress-widget');
+    }
+
+    window.addEventListener("message", function(event) {
+      if (event.data.name === 'webchatLoaded') {
+        // Do nothing
+      } else if (event.data.message_type === "visit") {
+        // Reset session
+        window.botpressWebChat.sendEvent({ type: "session_reset" }) 
+
+        const thisScript = getScript();
+        if (thisScript) {
+          // Build first event object
+          const firstEvent = {}
+
+          const pageType = thisScript.getAttribute('pageType') || 'homePage';
+          if (pageType === 'loginPage') {
+            firstEvent.type = pageType
+          } else {
+            // Read Keycloak token
+            const subject = thisScript.getAttribute('subject');
+            if (subject) {
+              // Connected
+              firstEvent.type = "keycloak_connect"
+              firstEvent.value = subject
+            } else {
+              firstEvent.type = "keycloak_disconnect"
+            }
+          }
+
+          // Send first event that is going to trigger welcome message
+          window.botpressWebChat.sendEvent(firstEvent)
+        }
+      }
+    })
+  }
+})
+
+window.addEventListener("load", function() {
     if (window.botpressWebChat) {
       window.botpressWebChat.init({
         host: "https://botpress.apps.ocp.lab-nxtit.com",
