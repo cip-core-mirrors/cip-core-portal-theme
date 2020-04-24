0fb3a0353ee56209c4522279e1362607e106116b
Mathieu <mathieu.0598@gmail.com>
2020-04-24 14:35:58 +0200

    Update chatbot login process

--- assets/js/botpress.js
+++ assets/js/botpress.js
@@ -5,40 +5,18 @@ window.addEventListener("load", function() {
       botId: "botpress-stable",
     })
 
-    function getScript() {
-      return document.getElementById('botpress-widget');
-    }
-
+    let subject;
     window.addEventListener("message", function(event) {
-      if (event.data.name === 'webchatLoaded') {
+      if (event.data.subject) {
+        console.log("ok sub chat")
+        subject = event.data.subject;
+      } else if (event.data.name === 'webchatLoaded') {
         // Do nothing
       } else if (event.data.message_type === "visit") {
         // Reset session
-        window.botpressWebChat.sendEvent({ type: "session_reset" }) 
+        window.botpressWebChat.sendEvent({ type: "session_reset" })
 
-        const thisScript = getScript();
-        if (thisScript) {
-          // Build first event object
-          const firstEvent = {}
-
-          const pageType = thisScript.getAttribute('pageType') || 'homePage';
-          if (pageType === 'loginPage') {
-            firstEvent.type = pageType
-          } else {
-            // Read Keycloak token
-            const subject = thisScript.getAttribute('subject');
-            if (subject) {
-              // Connected
-              firstEvent.type = "keycloak_connect"
-              firstEvent.value = subject
-            } else {
-              firstEvent.type = "keycloak_disconnect"
-            }
-          }
-
-          // Send first event that is going to trigger welcome message
-          window.botpressWebChat.sendEvent(firstEvent)
-        }
+        window.botpressWebChat.sendEvent(subject ? {type: 'keycloak_connect', value: subject}: {type: 'keycloak_disconnect'})
       }
     })
   }
